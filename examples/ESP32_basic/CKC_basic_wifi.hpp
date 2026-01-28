a1c44f5b493a080e0e09c7194163569068278d37
PLGdevo <minhphong270704@gmail.com>
2026-01-28 16:45:49 +0700

    PLG_update_ver_1.2.3

---
+++ examples/ESP32_basic/CKC_basic_wifi.hpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#define CKC_DEBUG
+#include <CKC.h>
+
+CKC CKC_IoT;
+void setup()
+{
+  Serial.begin(115200);
+  CKC_IoT.begin("PLG", "admin123");
+  delay(20000);
+}
+void loop()
+{
+  static unsigned long sendTimer = 0;
+  if (millis() - sendTimer > 3000)
+  {
+    sendTimer = millis();
+    CKC_IoT.sendDATA("1", "2", "3", "4", "5");
+  }
+}
---
+++ src/ESP32_simple.hpp
@@ -0,0 +1,10 @@
+#ifndef ESP32_simple
+#define ESP32_simple
+
+#include <CKC_WiFi/CKC_PnP_ESP32.hpp>
+#include <CKC/CKC_Type/CKC_debug.hpp>
+
+
+
+
+#endif
